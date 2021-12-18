e5009591e480311900555b1d81ec6326b3a3a1ba
Hieu Dao <hieush0899@gmail.com>
2021-12-18 10:11:37 +0700

    new pages

--- querySQL/device_control.sql
+++ querySQL/device_control.sql
@@ -1,3 +1,6 @@
 SELECT * FROM mydatabase.device_control;
 
 update device_control set Device1 = 0 where Room = 'kitchen';
+insert into device_control (Room, Device1, Device2,Device3,Device4) values ("kitchen", 1, 1, 1, 1) on duplicate key update
+Room = values(Room), Device1=values(Device1), Device2=values(Device2), Device3=values(Device3);
+select * from device_control where (Device1 = 1 or Device2 = 1 or Device3 = 1 or Device4 = 1);
